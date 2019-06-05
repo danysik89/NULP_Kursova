302e35f8091925a4ebc4c67c45ba25f81367976c
Lenovo-PC\Danylo <danilogoryachko@gmail.com>
2019-06-06 00:47:00 +0300

    delete usless

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,19 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { AlbumsComponent } from './albums/albums.component';
 import { AlbumsService } from './albums.service';
-import { PhotosComponent } from './photos/photos.component';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
-import { PhotoInfoComponent } from './photo-info/photo-info.component';
 
 const appRoutes: Routes = [
   {path: '', component: MainComponent},
-  {path: 'albums', component: AlbumsComponent},
-  {path: 'albums/:id', component: PhotosComponent},
-  {path: 'albums/:id/:idPhoto', component: PhotoInfoComponent}
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlbumsComponent,
-    PhotosComponent,
     MainComponent,
-    PhotoInfoComponent
   ],
   imports: [
     BrowserModule,
